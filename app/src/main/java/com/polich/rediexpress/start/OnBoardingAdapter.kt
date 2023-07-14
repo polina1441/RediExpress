caf8438215e17748f6113ab6a56a1a0b2c3ac148
Polina1441 <14042003dp@mail.ru>
2023-07-14 19:17:51 +0300

    show last button

--- app/src/main/java/com/polich/rediexpress/start/OnBoardingAdapter.kt
+++ app/src/main/java/com/polich/rediexpress/start/OnBoardingAdapter.kt
@@ -1,6 +1,5 @@
 package com.polich.rediexpress.start
 
-import android.opengl.Visibility
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,7 +8,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.polich.rediexpress.R
 import com.polich.rediexpress.databinding.ItemOnboardingBinding
 
-class OnBoardingAdapter(private val onBoardings: Array<OnBoarding>):
+class OnBoardingAdapter(private val onBoardings: Array<OnBoarding>, val showLastButton: (Boolean) -> Unit,):
     RecyclerView.Adapter<OnBoardingAdapter.OnBoardingViewHolder>(){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OnBoardingViewHolder {
@@ -19,7 +18,7 @@ class OnBoardingAdapter(private val onBoardings: Array<OnBoarding>):
     }
 
     override fun onBindViewHolder(holder: OnBoardingViewHolder, position: Int) {
-        holder.bind(onBoardings[position])
+        holder.bind(onBoardings[position], position)
     }
 
     override fun getItemCount() = onBoardings.size
@@ -27,18 +26,17 @@ class OnBoardingAdapter(private val onBoardings: Array<OnBoarding>):
     inner class OnBoardingViewHolder(view: View): RecyclerView.ViewHolder(view){
         private val binding = ItemOnboardingBinding.bind(view)
 
-        fun bind(onBoarding: OnBoarding){
+        fun bind(onBoarding: OnBoarding, position: Int){
             binding.imageView.setImageResource(onBoarding.img)
             binding.textBodyOnb.text = onBoarding.text
             binding.titleOnb.text = onBoarding.title
-            if (onBoarding.viewSignUp){
-                binding.lastPageButton.visibility = View.VISIBLE
-            } else {
-                binding.lastPageButton.visibility = View.GONE
-            }
+
+            if (position == onBoardings.lastIndex) {
+                showLastButton(true)
+            } else showLastButton(false)
         }
     }
 }
 
 
-data class OnBoarding(val title : String, val text: String, @DrawableRes val img: Int, val viewSignUp: Boolean)
+data class OnBoarding(val title : String, val text: String, @DrawableRes val img: Int)
--- app/src/main/java/com/polich/rediexpress/start/OnBoardingFragment.kt
+++ app/src/main/java/com/polich/rediexpress/start/OnBoardingFragment.kt
@@ -2,23 +2,24 @@ package com.polich.rediexpress.start
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.polich.rediexpress.R
 import com.polich.rediexpress.databinding.FragmentOnBoardingBinding
 
 class OnBoardingFragment : Fragment(R.layout.fragment_on_boarding) {
     private val onBoardings : Array<OnBoarding> = arrayOf(
-        OnBoarding("", "Enjoy quick pick-up and delivery to your destination", R.drawable.onb1, false),
-        OnBoarding("Flexible Payment", "Different modes of payment either before and after delivery without stress", R.drawable.onb2, false),
-        OnBoarding("Real-time Tracking", "", R.drawable.onb3, true),
+        OnBoarding("", "Enjoy quick pick-up and delivery to your destination", R.drawable.onb1, ),
+        OnBoarding("Flexible Payment", "Different modes of payment either before and after delivery without stress", R.drawable.onb2, ),
+        OnBoarding("Real-time Tracking", "", R.drawable.onb3, ),
     )
+
+    lateinit var binding : FragmentOnBoardingBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val binding = FragmentOnBoardingBinding.bind(view)
+        binding = FragmentOnBoardingBinding.bind(view)
         binding.viewPageOnboarding.adapter = OnBoardingAdapter(onBoardings)
+            { showLastButton(it) }
         binding.dots.attachTo(binding.viewPageOnboarding)
 
         binding.skipOnb.setOnClickListener {
@@ -28,6 +29,16 @@ class OnBoardingFragment : Fragment(R.layout.fragment_on_boarding) {
         binding.nextOnb.setOnClickListener {
             val page = binding.viewPageOnboarding.getCurrentItem()
             binding.viewPageOnboarding.setCurrentItem(page+1)
+        }
+    }
+
+    fun showLastButton(isVisible: Boolean){
+        if (isVisible){
+            binding.lastPageButton.visibility = View.VISIBLE
+            binding.buttons.visibility = View.GONE
+        } else {
+            binding.lastPageButton.visibility = View.GONE
+            binding.buttons.visibility = View.VISIBLE
         }
     }
 }
